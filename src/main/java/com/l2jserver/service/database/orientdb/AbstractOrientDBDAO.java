48273476c08d4202829c6d75315a472c13fedc6a
Rogiel <rogiel@rogiel.com>
2011-09-19 15:51:52 -0300

    Add support for OrientDatabaseService
    OrientDB is an object database. Data is stored in a pair of key-value
    and can be retrieved or filtered using any of those keys. Data is stored
    at 'data/database' and has been added to .gitignore.

---
+++ src/main/java/com/l2jserver/service/database/orientdb/AbstractOrientDBDAO.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of l2jserver <l2jserver.com>.
+ *
+ * l2jserver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * l2jserver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with l2jserver.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.l2jserver.service.database.orientdb;
+
+import com.l2jserver.model.Model;
+import com.l2jserver.model.id.ID;
+import com.l2jserver.service.database.AbstractDAO;
+import com.l2jserver.service.database.DatabaseService;
+import com.l2jserver.service.database.OrientDatabaseService;
+
+/**
+ * @author <a href="http://www.rogiel.com">Rogiel</a>
+ * @param <T>
+ *            the model type
+ * @param <I>
+ *            the id type
+ */
+public abstract class AbstractOrientDBDAO<T extends Model<?>, I extends ID<?>>
+		extends AbstractDAO<T, I> {
+	protected final OrientDatabaseService database;
+
+	/**
+	 * @param database
+	 *            the database instance
+	 */
+	protected AbstractOrientDBDAO(DatabaseService database) {
+		super(database);
+		this.database = (OrientDatabaseService) database;
+	}
+}
