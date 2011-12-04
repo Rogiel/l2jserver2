f959661eff687c2d7bd69acfa32648473c851832
Rogiel <rogiel@rogiel.com>
2011-12-04 13:09:53 -0200

    Fix duplicated maven dependencies

--- l2jserver2-gameserver/src/main/java/com/l2jserver/service/game/world/event/WorldEventDispatcherImpl.java
+++ l2jserver2-gameserver/src/main/java/com/l2jserver/service/game/world/event/WorldEventDispatcherImpl.java
@@ -284,7 +284,7 @@ public class WorldEventDispatcherImpl implements WorldEventDispatcher {
 				return false;
 			if (complete)
 				return false;
-			return cancel();
+			return cancel(mayInterruptIfRunning);
 		}
 
 		@Override
